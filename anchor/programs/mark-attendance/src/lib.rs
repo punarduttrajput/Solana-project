8a7348ab1136709dfd132647f7ce137d23a02715
punarduttrajput <punarduttrajput@gmail.com>
2024-03-12 22:17:22 +0530

    added mark attendance sc

--- anchor/programs/mark-attendance/src/lib.rs
+++ anchor/programs/mark-attendance/src/lib.rs
@@ -1,3 +1,20 @@
+// use anchor_lang::prelude::*;
+
+// declare_id!("6TfP6s4vbSHGuT3R5yZYJ4keDUWhbY8LtUF69Hotbwn4");
+
+// #[program]
+// pub mod mark_attendance {
+//     use super::*;
+
+//     pub fn greet(_ctx: Context<Initialize>) -> Result<()> {
+//         msg!("GM!");
+//         Ok(())
+//     }
+// }
+
+// #[derive(Accounts)]
+// pub struct Initialize {}
+
 use anchor_lang::prelude::*;
 
 declare_id!("6TfP6s4vbSHGuT3R5yZYJ4keDUWhbY8LtUF69Hotbwn4");
@@ -6,11 +23,15 @@ declare_id!("6TfP6s4vbSHGuT3R5yZYJ4keDUWhbY8LtUF69Hotbwn4");
 pub mod mark_attendance {
     use super::*;
 
-    pub fn greet(_ctx: Context<Initialize>) -> Result<()> {
-        msg!("GM!");
+    #[derive(Accounts)]
+    pub struct MarkAttendance<'info> {
+        #[account(signer)]
+        pub employee: AccountInfo<'info>,
+    }
+
+    pub fn mark_attendance(ctx: Context<MarkAttendance>) -> Result<()> {
+        msg!("Attendance marked by employee: {}", ctx.accounts.employee.key());
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
